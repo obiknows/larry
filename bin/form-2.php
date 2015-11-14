41d7498bf4ce2c490d1e8a418f1ec3de9e134549
Sam Nnodim <sam.nnodim@gmail.com>
2015-11-14 07:03:34 -0500

    Initial commit, added blank README

---
+++ bin/form-2.php
@@ -0,0 +1,23 @@
+<?php	
+	if(empty($_POST['name2']) || empty($_POST['name2']) || empty($_POST['email2']) || empty($_POST['email2']))
+	{
+		return false;
+	}
+	
+	$name2 = $_POST['name2'];
+	$name2 = $_POST['name2'];
+	$email2 = $_POST['email2'];
+	$email2 = $_POST['email2'];
+	
+	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Message from My Site.";
+	$email_body = "You have received a new message. \n\n".
+				  "Name2: $name2 \nName2: $name2 \nEmail2: $email2 \nEmail2: $email2 \n";
+	$headers = "From: contact@yoursite.com\n";
+	$headers .= "Reply-To: $email2";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
